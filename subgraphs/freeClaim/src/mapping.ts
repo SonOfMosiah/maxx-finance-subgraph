f86616843b3c4399d81611eddedce249f02ddeea
SonOfMosiah <84041746+SonOfMosiah@users.noreply.github.com>
2022-11-27 12:15:24 -0700

    Update mapping.ts

--- subgraphs/freeClaim/src/mapping.ts
+++ subgraphs/freeClaim/src/mapping.ts
@@ -1,117 +1,103 @@
 import { Address, BigInt, ethereum, dataSource } from '@graphprotocol/graph-ts';
 
-import * as amplifier from '../generated/LiquidityAmplifier/LiquidityAmplifier';
+import * as freeClaim from '../generated/FreeClaim/FreeClaim';
 
-import { SetStakeAddressCall } from '../generated/LiquidityAmplifier/LiquidityAmplifier';
-
-import {
-  LiquidityAmplifier,
-  Day,
-  Participant,
-  Deposit,
-  Referral,
-  Claim,
-} from '../generated/schema';
+import { FreeClaim, Referral, Referrer, Claim } from '../generated/schema';
 
 const ADDRESS_ZERO = Address.fromString(
   '0x0000000000000000000000000000000000000000'
 );
 
-export function handleDeposit(event: amplifier.Deposit): void {
-  const contract = amplifier.LiquidityAmplifier.bind(dataSource.address());
+export function handleUserClaim(event: freeClaim.UserClaim): void {
+  const contract = freeClaim.FreeClaim.bind(dataSource.address());
 
-  let depositId = event.transaction.hash.toHex();
-  let deposit = new Deposit(depositId);
-
-  let participant = getParticipant(event.params.user.toHex());
-
-  deposit.participant = event.params.user.toHex();
-  deposit.amount = event.params.amount;
-  if (event.params.referrer != ADDRESS_ZERO) {
-    createReferral(event);
-    deposit.effectiveAmount = event.params.amount;
-    //   .times(BigInt.fromI32(11))
-    //   .div(BigInt.fromI32(10));
-  } else {
-    deposit.effectiveAmount = event.params.amount;
-  }
-
-  let day = getDay(contract.getDay());
-  day.maticDeposits = day.maticDeposits.plus(deposit.amount);
-  day.effectiveMaticDeposits = day.effectiveMaticDeposits.plus(
-    deposit.effectiveAmount
-  );
-  deposit.day = day.id;
-  deposit.save();
-}
-
-export function handleClaim(event: amplifier.Claim): void {
-  const contract = amplifier.LiquidityAmplifier.bind(dataSource.address());
-  const claimId = event.transaction.hash.toHex();
+  let claimId =
+    event.transaction.hash.toHex() + '-' + event.params.user.toHex();
   let claim = new Claim(claimId);
 
-  claim.participant = event.params.user.toHex();
+  claim.user = event.params.user;
   claim.amount = event.params.amount;
-
   claim.save();
 }
 
-function getLiquidityAmplifier(): LiquidityAmplifier {
-  const contract = amplifier.LiquidityAmplifier.bind(dataSource.address());
-
-  let liquidityAmplifier = LiquidityAmplifier.load(
-    dataSource.address().toHex()
+export function handleReferral(event: freeClaim.Referral): void {
+  const contract = freeClaim.FreeClaim.bind(dataSource.address());
+  const referralId = event.transaction.hash.toHex();
+  let referral = new Referral(referralId);
+  let referrer = getReferrer(event.params.referrer.toHex());
+  referrer.totalReferrals = referrer.totalReferrals.plus(BigInt.fromI32(1));
+  referrer.totalReferralAmount = referrer.totalReferralAmount.plus(
+    event.params.amount
   );
-  if (liquidityAmplifier == null) {
-    liquidityAmplifier = new LiquidityAmplifier(dataSource.address().toHex());
-    liquidityAmplifier.launchDate = contract.launchDate();
-    liquidityAmplifier.maxxVault = contract.maxxVault();
-    liquidityAmplifier.maxx = contract.maxx();
-    liquidityAmplifier.stake = contract.stake();
-    liquidityAmplifier.save();
-  }
+  referrer.save();
 
-  return liquidityAmplifier as LiquidityAmplifier;
-}
+  let claim = getClaim(
+    event.transaction.hash.toHex() + '-' + event.params.user.toHex()
+  );
+  claim.referrer = event.params.user.toHex();
+  claim.save();
 
-function getDay(dayId: number): Day {
-  const contract = amplifier.LiquidityAmplifier.bind(dataSource.address());
-
-  let day = Day.load(dayId.toString());
-  if (day == null) {
-    day = new Day(dayId.toString());
-    // day.maxxAllocation = contract.getMaxxDailyAllocation(
-    //   BigInt.fromString(dayId.toString()).toI32()
-    // );
-    day.maticDeposits = BigInt.fromI32(0);
-    day.effectiveMaticDeposits = BigInt.fromI32(0);
-    day.save();
-  }
-
-  return day as Day;
-}
-
-function getParticipant(id: string): Participant {
-  let participant = Participant.load(id);
-  if (participant == null) {
-    participant = new Participant(id);
-    // participant.dailyDeposits = [];
-    // participant.effectiveDailyDeposits = [];
-    // participant.effectiveUserReferrals = [];
-    participant.participated = true;
-    participant.save();
-  }
-  return participant as Participant;
-}
-
-function createReferral(event: amplifier.Deposit): void {
-  let id = event.transaction.hash.toHex();
-  let referral = new Referral(id);
-  referral.referral = event.params.user.toHex();
-  referral.referrer = event.params.referrer.toHex();
+  referral.referrer = event.params.referrer;
+  referral.referral = event.params.user;
   referral.referredAmount = event.params.amount;
-  referral.effectiveReferredAmount = event.params.amount;
-  // .times(BigInt.fromI32(11))
-  // .div(BigInt.fromI32(10));
+  referral.timestamp = event.block.timestamp;
   referral.save();
+}
+
+export function handleMaxxSet(event: freeClaim.MaxxSet): void {
+  let freeClaim = getFreeClaim();
+  freeClaim.maxx = event.params.maxx;
+  freeClaim.save();
+}
+
+export function handleMerkleRootSet(event: freeClaim.MerkleRootSet): void {
+  let freeClaim = getFreeClaim();
+  freeClaim.merkleRoot = event.params.merkleRoot;
+  freeClaim.save();
+}
+
+export function handleLaunchDateUpdated(
+  event: freeClaim.LaunchDateUpdated
+): void {
+  let freeClaim = getFreeClaim();
+  freeClaim.launchDate = event.params.launchDate;
+  freeClaim.save();
+}
+
+function getFreeClaim(): FreeClaim {
+  const contract = freeClaim.FreeClaim.bind(dataSource.address());
+
+  let freeClaim = FreeClaim.load(dataSource.address().toHex());
+  if (freeClaim == null) {
+    freeClaim = new FreeClaim(dataSource.address().toHex());
+    freeClaim.launchDate = contract.launchDate();
+    freeClaim.merkleRoot = contract.merkleRoot();
+    freeClaim.maxx = contract.maxx();
+    freeClaim.totalClaimed = BigInt.fromI32(0);
+    freeClaim.totalReferralAmount = BigInt.fromI32(0);
+    freeClaim.save();
+  }
+
+  return freeClaim as FreeClaim;
+}
+
+function getReferrer(id: string): Referrer {
+  let referrer = Referrer.load(id);
+  if (referrer == null) {
+    referrer = new Referrer(id);
+    referrer.totalReferrals = BigInt.fromI32(0);
+    referrer.totalReferralAmount = BigInt.fromI32(0);
+  }
+  return referrer as Referrer;
+}
+
+function getClaim(id: string): Claim {
+  let claim = Claim.load(id);
+  if (claim == null) {
+    claim = new Claim(id);
+    claim.user = ADDRESS_ZERO;
+    claim.amount = BigInt.fromI32(0);
+    claim.save();
+  }
+  return claim as Claim;
 }
